2a4d107c6824d3b90b34c1a031385950ff1d23b8
Jake <linweichen08@gmail.com>
2020-09-30 09:26:59 +1000

    How to create and validate forms in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'form_screen.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coding with Curry',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: FormScreen(),
+    );
+  }
+}
+
 
 
